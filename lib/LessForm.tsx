9ceeff59dcf3f42c2c13a3b60561d9d9a66a261d
pillar <ymblender@gmail.com>
2022-06-14 15:24:25 +0800

    fix: form 变成 form标签

--- lib/LessForm.tsx
+++ lib/LessForm.tsx
@@ -1,10 +1,35 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { SignleContext } from "./SingleContext";
+import { FormProps } from "./types";
 import { FormContext } from "./useForm";
 
 const SingleProvider = SignleContext.Provider;
 
-// not have form children
-export function LessForm(props: { value: FormContext<any>; children: any }) {
-  return <SingleProvider {...props}></SingleProvider>;
+export type LessFormProps = FormProps<{
+  name?: string;
+  value: FormContext<any>;
+  children: any;
+}>;
+
+export function LessForm({
+  children,
+  onSubmit,
+  value,
+  ...rest
+}: LessFormProps) {
+  return (
+    <SingleProvider value={value}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (onSubmit) {
+            onSubmit(e);
+          }
+        }}
+        {...rest}
+      >
+        {children}
+      </form>
+    </SingleProvider>
+  );
 }
