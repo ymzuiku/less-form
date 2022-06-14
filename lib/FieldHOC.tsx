cb3461b220da961329774d32622d07e378ce9749
pillar <ymblender@gmail.com>
2022-06-15 01:54:42 +0800

    fix: use pnpm

--- lib/FieldHOC.tsx
+++ lib/FieldHOC.tsx
@@ -1,12 +1,28 @@
+import { Override } from "./types";
 import { LoadType, useField } from "./useField";
 
+type FieldHOCProps<T> = Override<
+  T,
+  {
+    name: string;
+    loadType?: LoadType;
+    onChange?: (val: any) => void;
+  }
+>;
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function FieldHOC<T extends { name?: string; loadType?: LoadType }>(
-  Component: any
-) {
-  return function FieldHOCComponent({ name, loadType, ...rest }: T) {
+export function FieldHOC<T>(Component: any) {
+  return function FieldHOCComponent({
+    name,
+    loadType,
+    onChange,
+    ...rest
+  }: FieldHOCProps<T>) {
     if (name) {
       const ctx = useField(name, loadType);
+      if (onChange) {
+        onChange(ctx.value);
+      }
       return <Component {...ctx} {...rest} />;
     }
 
